ad9aad206bcd7371c945481b527d8b6351f6d4fb
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-03-06 21:30:38 +0100

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Alpine Ridge
+
+A proprietary in-house toolkit for creating interactive web experiences.
+
+## Statement
+
+Ridge is not a framework but a minimalistic toolkit that has a strong focus on creating progressivily enhanced interactive experiences on a wide range of consumer devices. The goal of Ridge is to empower the developer to focus on the creative process and less on implementation details and repetitive work and browser specific workarounds. The components in the core of the library are things you will likely need when developing an interactive experience, handles edge-cases (like vendor prefixing, experimental feature support testing and fallbacks) or things that inspire the developer to use more advanced optimisations that one normally does not have the time to implement and test under short deadlines.
+
+## Browser support
+
+Ridge supports the latest two versions of `evergreen` browsers and `Internet Explorer 11`:
+
+- Edge
+- Firefox
+- Chrome
+- Chrome Android
+- Safari
+- iOS Safari
+
+Ridge does not include any polyfills. It is recommended to use the [polyfill.io](https://polyfill.io/v3/) service in your application.
+
+## Licence
+
+The Ridge toolkit is proprietary software.
---
+++ rollup.config.ts
@@ -0,0 +1,74 @@
+// Vendor
+// @ts-ignore: missing type definition
+import commonjsPlugin from 'rollup-plugin-commonjs';
+// @ts-ignore: missing type definition
+import filesizePlugin from 'rollup-plugin-filesize';
+// @ts-ignore: missing type definition
+import resolvePlugin from 'rollup-plugin-node-resolve';
+import typescriptPlugin from 'rollup-plugin-typescript2';
+// @ts-ignore: missing type definition
+import { uglify as uglifyPlugin } from 'rollup-plugin-uglify';
+
+// Package
+// @ts-ignore: JSON is imported without any issue, TSLint still raises issues
+import pkg from './package.json';
+
+const input = './src/index.ts';
+const name = 'Ridge';
+
+const plugins = ({ isUMD = false, isCJS = false, isES = false }) => [
+  resolvePlugin(),
+  (isUMD || isCJS) && commonjsPlugin(),
+  typescriptPlugin({
+    typescript: require('typescript'),
+    useTsconfigDeclarationDir: true,
+  }),
+  !isES && !process.env.ROLLUP_WATCH && uglifyPlugin(),
+  !isES && !process.env.ROLLUP_WATCH && filesizePlugin(),
+];
+
+export default [
+  {
+    input,
+    output: [
+      {
+        exports: 'named',
+        file: pkg.browser,
+        format: 'umd',
+        name,
+      },
+    ],
+    plugins: plugins({ isUMD: true }),
+    watch: {
+      include: 'src/**',
+    },
+  },
+  {
+    input,
+    output: [
+      {
+        exports: 'named',
+        file: pkg.main,
+        format: 'cjs',
+      },
+    ],
+    plugins: plugins({ isCJS: true }),
+    watch: {
+      include: 'src/**',
+    },
+  },
+  {
+    input,
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        name,
+      },
+    ],
+    plugins: plugins({ isES: true }),
+    watch: {
+      include: 'src/**',
+    },
+  },
+];
