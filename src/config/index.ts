0e0be9e6aa0964f6843927e510e74bf528cd52f4
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-03-06 21:48:08 +0100

    added config

---
+++ src/config/index.ts
@@ -0,0 +1,26 @@
+// @ts-check
+
+/**
+ * Acts like a global configuration object but avoids attaching itself to the window object
+ */
+export const config = {
+  LOG_VERBOSITY: 0, // [0 - 3]
+};
+
+/**
+ * Gets a config entry
+ *
+ * @param {string} key Key
+ */
+// @ts-ignore
+export const getConfigEntry = (key: string) => config[key];
+
+/**
+ * Sets a config entry
+ *
+ * @param {string} key Key
+ * @param {string|boolean|number} value Value
+ */
+export const setConfigEntry = (key: string, value: string | boolean | number) =>
+  // @ts-ignore
+  (config[key] = value);
