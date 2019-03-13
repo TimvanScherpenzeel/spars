6bb8a2e5a06c24aa5c6295591a19a5d2bb4b9b9b
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-03-13 20:27:19 +0100

    fix comments

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,7 +1,8 @@
 // Config
 import { getConfigEntry } from '../config';
+
 /**
- * console.log a message to the console in a prefixed format
+ * console.log() a message to the console in a prefixed format
  * Only logs when the highest verbosity level is set
  *
  * @param message Message to log in the console
@@ -10,7 +11,7 @@ export const log = (message: string) =>
   getConfigEntry('LOG_VERBOSITY') >= 3 && console.log(`Ridge :: [LOG] -> ${message}`);
 
 /**
- * console.warn a message to the console in a prefixed format
+ * console.warn() a message to the console in a prefixed format
  * Only logs when the medium or highest verbosity level is set
  *
  * @param message Message to warn in the console
@@ -19,7 +20,7 @@ export const warn = (message: string) =>
   getConfigEntry('LOG_VERBOSITY') >= 2 && console.warn(`Ridge :: [WARN] -> ${message}`);
 
 /**
- * console.error a message to the console in a prefixed format
+ * console.error() a message to the console in a prefixed format
  * Only logs when the low, medium or highest verbosity level is set
  * Gets disabled if verbosity is set to 0 (usually the case in production)
  *
