337b0d35272f58875b0779f1a759c9c2b4e06cd7
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-03-18 20:31:28 +0100

    note mitt

--- src/events/eventEmitter.ts
+++ src/events/eventEmitter.ts
@@ -1,10 +1,9 @@
 // Vendor
 import Mitt from 'mitt';
 
-// An extremely simple event emitter singleton allowing global event handling
-
-// https://github.com/developit/mitt
-
+/**
+ * Inspired by https://github.com/developit/mitt
+ */
 const eventEmitter = new Mitt();
 
 export { eventEmitter, Mitt as EventEmitter };
