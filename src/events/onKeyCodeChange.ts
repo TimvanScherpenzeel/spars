a9bf16e7a896376721f9ee3825f10272cdfd53a7
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-07-19 11:56:12 +0200

    lots of bugs

--- src/events/onKeyCodeChange.ts
+++ src/events/onKeyCodeChange.ts
@@ -4,12 +4,10 @@ import { eventEmitter } from './EventEmitter';
 // Add polyfill for keycode
 
 // Issues:
-// Chrome Android returns undefined keycodes using `.key` and always 229 on `.keyCode` (using the native keyboard) - https://live.browserstack.com/dashboard#os=android&os_version=9.0&device=Samsung+Galaxy+S10e&device_browser=chrome&zoom_to_fit=true&full_screen=true&url=http%3A%2F%2Flocalhost%3A1234%2F&speed=1&host_ports=google.com%2C80%2C0
+// Chrome Android always return 229 on `.keyCode` (see: https://stackoverflow.com/a/41517115)
+// Chrome Android returns `Unidentified` keycodes using `.key` (using the stock native keyboard on Samsung / Pixel) - https://live.browserstack.com/dashboard#os=android&os_version=9.0&device=Samsung+Galaxy+S10e&device_browser=chrome&zoom_to_fit=true&full_screen=true&url=http%3A%2F%2Flocalhost%3A1234%2F&speed=1&host_ports=google.com%2C80%2C0
 // Internet Explorer 11 uses `Up`, `Left`, `Down`, `Right` instead of `ArrowUp`, ArrowLeft`, `ArrowDown`, `ArrowRight` (Chrome, Safari, Edge, Firefox)
-// `Spacebar` in Safari does not work
-
-// Correct:
-// iOS Safari / Chrome (12 / 11 / 10)
+// Everywhere: `Spacebar` key does not work, keyCode does (32)
 
 /**
  * Monitor keycode changes
