8c9b027eb9203d0cbf841fc29db20ab3e2a7f283
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-07-22 14:44:25 +0200

    clean up

--- src/sensors/accelerometer/math.ts
+++ src/sensors/accelerometer/math.ts
@@ -135,38 +135,6 @@ export class Quaternion {
     return this;
   }
 
-  public setFromEulerXYZ(x: number, y: number, z: number): Quaternion {
-    const c1 = Math.cos(x / 2);
-    const c2 = Math.cos(y / 2);
-    const c3 = Math.cos(z / 2);
-    const s1 = Math.sin(x / 2);
-    const s2 = Math.sin(y / 2);
-    const s3 = Math.sin(z / 2);
-
-    this.x = s1 * c2 * c3 + c1 * s2 * s3;
-    this.y = c1 * s2 * c3 - s1 * c2 * s3;
-    this.z = c1 * c2 * s3 + s1 * s2 * c3;
-    this.w = c1 * c2 * c3 - s1 * s2 * s3;
-
-    return this;
-  }
-
-  public setFromEulerYXZ(x: number, y: number, z: number): Quaternion {
-    const c1 = Math.cos(x / 2);
-    const c2 = Math.cos(y / 2);
-    const c3 = Math.cos(z / 2);
-    const s1 = Math.sin(x / 2);
-    const s2 = Math.sin(y / 2);
-    const s3 = Math.sin(z / 2);
-
-    this.x = s1 * c2 * c3 + c1 * s2 * s3;
-    this.y = c1 * s2 * c3 - s1 * c2 * s3;
-    this.z = c1 * c2 * s3 - s1 * s2 * c3;
-    this.w = c1 * c2 * c3 + s1 * s2 * s3;
-
-    return this;
-  }
-
   public setFromAxisAngle(axis: Vector3, angle: number): Quaternion {
     // http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToQuaternion/index.htm
     // assumes axis is normalized
