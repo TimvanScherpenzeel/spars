d7e8a8d25b514102730bf07ac275fdce6ed409f6
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-07-29 19:41:13 +0200

    export ambient light sensor

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ export { fullScreen, pointerLock } from './polyfills';
 export { priorities, scheduleFrame } from './frameScheduler';
 
 // Sensors
-export { geolocationSensor, relativeOrientationSensor } from './sensors';
+export { ambientLightSensor, geolocationSensor, relativeOrientationSensor } from './sensors';
 
 // ThreadPool
 export { threadPool } from './threadPool';
--- src/sensors/index.ts
+++ src/sensors/index.ts
@@ -1,3 +1,4 @@
 // Sensors
+export { ambientLightSensor } from './ambientLightSensor';
 export { geolocationSensor } from './geolocationSensor';
 export { relativeOrientationSensor } from './relativeOrientationSensor';
