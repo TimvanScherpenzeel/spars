0068d393c83fb9971282daace05b1470a509708a
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-07-30 14:38:02 +0200

    fade in fade out

--- src/audio/AudioManager.ts
+++ src/audio/AudioManager.ts
@@ -1,13 +1,14 @@
 // Cookie
 import { setCookie } from '../cookie';
 
+// Global audio manager
 // Asset loader is responsible for loading the audio files, audio manager should just read them
 // Being able to globally mute the audio (store preference in cookie)
 // Track the memory load (buffer.duration of each buffered sound)
 
 // https://github.com/Wizcorp/AudioManager
 
-// Sound groops
+// Sound groups (scene)
 
 export class AudioManager {
   constructor() {}
@@ -19,6 +20,10 @@ export class AudioManager {
   public unmute(): void {
     setCookie('muteAudio', 'false');
   }
+
+  public fadeIn(): void {}
+
+  public fadeOut(): void {}
 
   public pause(): void {}
 
