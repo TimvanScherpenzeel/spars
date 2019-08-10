85ca47a3cdb9f996ce6c38e95f4fba547834104f
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-08-10 08:45:53 +0200

    improve empty parameter handling, default to true if no value was given for key

--- src/utilities/getQueryParameters.ts
+++ src/utilities/getQueryParameters.ts
@@ -1,7 +1,11 @@
 /**
  * Get search parameters from URL
  *
- * ?isDebug=false&isGUI=true would result in { isDebug: false, isGUI: true }
+ * ?isDebug=false&isGUI=true&pi=3.141592653589793 would result in { isDebug: false, isGUI: true, pi: 3.141592653589793 }
+ *
+ * ?isBackground&isBlack would result in { isBackground: true, isBlack: true }
+ *
+ * ?mainColor=blue&subColor=0xFFFFFF would result in { mainColor: "blue", subColor: 16777215 }
  *
  * @param url query parameters
  */
@@ -9,14 +13,22 @@ export const getQueryParameters = (url = window.location.search): { [parameter: 
   url
     // Remove ?
     .slice(1)
+
     // Split by &
     .split('&')
+
     // Find parameters
     .map(param => param.split('='))
+
     // Remove any empty string entries
-    .filter(param => param.length > 1)
+    .filter(param => param[0] !== '')
+
+    // If any keys didn't have a value, automatically use Boolean true for value
+    .map(param => (param.length === 1 ? [param.toString(), 'true'] : param))
+
     // Construct { key: value } pairs
     .reduce((values, [key, value]) => {
       (values as any)[key] = value;
+
       return values;
     }, {});
