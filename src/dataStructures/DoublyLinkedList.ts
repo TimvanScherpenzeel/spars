d22320b23b084452a79405bc7a36981a6ef000b1
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-08-20 17:30:18 +0200

    set up

--- src/dataStructures/DoublyLinkedList.ts
+++ src/dataStructures/DoublyLinkedList.ts
@@ -1,7 +1,30 @@
+// Types
 import { TNullable } from '../types';
+import { IDoublyLinkedListNode } from './types';
 
 export class DoublyLinkedList {
   private head: TNullable<IDoublyLinkedListNode> = null;
   private tail: TNullable<IDoublyLinkedListNode> = null;
   private length: number = 0;
+
+  /**
+   * Get the head of the list
+   */
+  get getHead(): TNullable<IDoublyLinkedListNode> {
+    return this.head;
+  }
+
+  /**
+   * Get the tail of the list
+   */
+  get getTail(): TNullable<IDoublyLinkedListNode> {
+    return this.tail;
+  }
+
+  /**
+   * Get length of the list
+   */
+  get getLength(): number {
+    return this.length;
+  }
 }
--- src/dataStructures/types/index.ts
+++ src/dataStructures/types/index.ts
@@ -5,3 +5,9 @@ export interface ISinglyLinkedListNode {
   value: () => void;
   next: TNullable<ISinglyLinkedListNode>;
 }
+
+export interface IDoublyLinkedListNode {
+  value: () => void;
+  previous: TNullable<IDoublyLinkedListNode>;
+  next: TNullable<IDoublyLinkedListNode>;
+}
