7c1de8c9ece23c170deb28a48c5ad6a8074dc68c
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-05-17 23:11:16 +0200

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    rules: {},
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/interface-name-prefix': [
+      'error',
+      {
+        prefixWithI: 'always',
+      },
+    ],
+  },
+  settings: {},
 };
